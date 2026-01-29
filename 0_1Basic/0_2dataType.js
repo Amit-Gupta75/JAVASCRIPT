1c0b48384a7ae925da930d30362389e53a0d6e56
Amit gupta <guptaamit7571@gmail.com>
2026-01-29 18:10:55 +0000

    learn data type

---
+++ 0_1Basic/0_2dataType.js
@@ -0,0 +1,16 @@
+"use strect"; // treat all js code as newer version
+
+let name = "amit"
+let age = 22
+let isloggedIn = false
+
+/*
+bigInt
+string
+boolean
+null=>standalone value
+undefined
+symbol
+object
+*/
+console.log(typeof(name));
